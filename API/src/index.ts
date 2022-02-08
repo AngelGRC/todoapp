10a5c362b6f49e6769275d4cae1101f562c4a80d
Angel Rincones <angelrincones97@gmail.com>
2022-02-08 01:51:02 -0300

    update package-json

--- API/src/index.ts
+++ API/src/index.ts
@@ -2,18 +2,17 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import * as express from 'express';
 import * as cors from 'cors';
-import * as helmet from 'helmet';
 import routes from './routes';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 createConnection()
   .then(async () => {
     // Create express app
     const app = express();
+
     // Middlewares
     app.use(cors());
-    app.use(helmet());
 
     app.use(express.json());
     // Routes
